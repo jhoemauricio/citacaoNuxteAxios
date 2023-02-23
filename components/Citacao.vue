b432b4f2ccd447bed69e815cd79054d2cc5ccf16
jhoemauricio <jhosephmauriciojhoseph@gmail.com git config --global user.name Your>
2023-02-23 13:47:15 -0300

    first commit

---
+++ components/Citacao.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="centralizar">
+    <h2 class="cor-texto">Citações</h2>
+    <div v-for="citacao in citacoes">
+      <!-- Citação -->
+      <b>"{{ citacao.quote }}"</b><br />
+      <p>{{ citacao.author }}</p>
+    </div>
+  </div>
+</template>
+
+
+<script>
+
+export default {
+  data() {
+    return {
+      citacoes: [],
+    };
+  },
+
+  methods: {
+    carregarFrases() {
+      this.$axios.get("https://dummyjson.com/quotes").then((response) => {
+        this.citacoes = response.data.quotes;
+      });
+    },
+  },
+  created() {
+    this.carregarFrases();
+  },
+};
+</script>
+
+
+
+<style scoped>
+.centralizar {
+  text-align: center;
+}
+.cor-texto {
+  color: red;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+<Citacao />
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
